8b796d7a2f00e0b8b1558af6d686656fe19105f3
MarcelDev <marceldev96@gmail.com>
2023-11-27 21:00:18 +0100

    nouveau checkout 6

--- routes/stripe.js
+++ routes/stripe.js
@@ -28,56 +28,11 @@ const checkoutSuccessPage = fs.readFileSync(
   });
 
 
-/*router.post("/create-checkout-session", async (req, res) => {
-  console.log("es tu la pour")
-  const customer = await stripe.customers.create({
-    metadata: {
-      userId: req.body.userId,
-      cart: JSON.stringify(req.body.cartItem),
-    },
-  });
-
- 
-  const line_items = req.body.cartItem.map((item) => {
-    return {
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: item.name,
-          imageUrl: item.image,
-          description: item.desc,
-          metadata: {
-            id: item.id,
-          },
-        },
-        unit_amount: item.price * 100,
-      },
-      quantity: item.cartQuantity,
-    };
-  });
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-   
-    
-    phone_number_collection: {
-      enabled: false,
-    },
-    line_items,
-    mode: "payment",
-    customer: customer.id,
-    success_url: "https://backend-stripe-payment-production.up.railway.app/stripe/checkout-success",
-    cancel_url:  "https://backend-stripe-payment-production.up.railway.app/stripe/cancel",
-  });
-
-  // res.redirect(303, session.url);
-  res.send({ url: session.url });
-});*/
 
 router.post('/create-checkout-session', async(req,res)=>{
   const session = await stripe.checkout.sessions.create({
     line_items:[
       {
-         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
          price: "pr_1234",
          quantity: 1,
       }
