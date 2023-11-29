c7ed57ab2e065a256b28efcecf8503f42bcde40b
MarcelDev <marceldev96@gmail.com>
2023-11-29 18:43:09 +0100

    checkout 1

--- routes/stripe.js
+++ routes/stripe.js
@@ -28,22 +28,141 @@ const checkoutSuccessPage = fs.readFileSync(
   });
 
 
+router.post("/create-checkout-session", async (req, res) => {
+  const customer = await stripe.customers.create({
+    metadata: {
+      userId: req.body.userId,
+      cart: JSON.stringify(req.body.cartItems),
+    },
+  });
 
-router.post('/create-checkout-session', async(req,res)=>{
+ 
+  const line_items = req.body.cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          imageUrl: item.image,
+          description: item.desc,
+          metadata: {
+            id: item.id,
+          },
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.cartQuantity,
+    };
+  });
   const session = await stripe.checkout.sessions.create({
-    line_items:[
-      {
-         price: "pr_1234",
-         quantity: 1,
-      }
-    ],
-    mode: 'payment',
-    success_url:  "https://backend-stripe-payment-production.up.railway.app/stripe/checkout-success",
-    cancel_url: "https://backend-stripe-payment-production.up.railway.app/stripe/cancel",
-  })
-  res.send({ url: session.url });
-})
+    payment_method_types: ["card"],
+   
+    
+    phone_number_collection: {
+      enabled: false,
+    },
+    line_items,
+    mode: "payment",
+    customer: customer.id,
+    success_url: "https://paymentorders-production.up.railway.app/stripe/checkout-success",
+    cancel_url:  "https://paymentorders-production.up.railway.app/stripe/cancel",
+  });
 
+  // res.redirect(303, session.url);
+  res.send({ url: session.url });
+});
+
+// Create order function
+
+const createOrder = async (customer, data) => {
+  const Items = JSON.parse(customer.metadata.cart);
+
+  const products = Items.map((item) => {
+    return {
+      productId: item.id,
+      quantity: item.cartQuantity,
+    };
+  });
+
+  const newOrder = new Order({
+    userId: customer.metadata.userId,
+    customerId: data.customer,
+    paymentIntentId: data.payment_intent,
+    products,
+    subtotal: data.amount_subtotal,
+    total: data.amount_total,
+    shipping: data.customer_details,
+    payment_status: data.payment_status,
+  });
+
+  try {
+    const savedOrder = await newOrder.save();
+    console.log("Processed Order:", savedOrder);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Stripe webhoook
+
+// router.post(
+//   "/webhook",
+//   bodyParser.raw({ type: "*/*" }),
+//   async (req, res) => {
+//     // req.rawBody = buf.toString();
+//     let data;
+//     let eventType;
+
+//     // Check if webhook signing is configured.
+//     let webhookSecret;
+//     webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+//     if (webhookSecret) {
+//       // Retrieve the event by verifying the signature using the raw body and secret.
+//       let event;
+//       let signature = req.headers["stripe-signature"];
+
+//       try {
+//         event = stripe.webhooks.constructEvent(
+//           req.body,
+//           signature,
+//           webhookSecret
+//         );
+//         console.log("Webhooks verified");
+//       } catch (err) {
+//         console.log(`⚠️  Webhook signature verification failed:  ${err}`);
+//         return res.sendStatus(400);
+//       }
+//       // Extract the object from the event.
+//       data = event.data.object;
+//       eventType = event.type;
+//     } else {
+//       // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+//       // retrieve the event data directly from the request body.
+//       data = req.body.data.object;
+//       eventType = req.body.type;
+//     }
+
+//     // Handle the checkout.session.completed event
+//     if (eventType === "checkout.session.completed") {
+//       console.log("Session Completed");
+//       // stripe.customers
+//       //   .retrieve(data.customer)
+//       //   .then(async (customer) => {
+//       //     try {
+//       //       // CREATE ORDER
+//       //       createOrder(customer, data);
+//       //     } catch (err) {
+//       //       console.log(typeof createOrder);
+//       //       console.log(err);
+//       //     }
+//       //   })
+//       //   .catch((err) => console.log(err.message));
+//     }
+
+//     res.status(200).end();
+//   }
+// );
 
 
 
